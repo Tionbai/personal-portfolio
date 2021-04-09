27e810f887793b75257daa15170be34567dcf2f9
Tionbai <post@bisgaard.no>
2021-04-09 13:45:24 +0200

    Updated packages and removed unused packages.

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
